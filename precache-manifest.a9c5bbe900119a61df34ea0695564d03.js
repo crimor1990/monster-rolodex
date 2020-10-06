3e40ff1eca927eef90e63a3fbdf8f7c802c42db6
Francesco Moscarelli <francesco90webdev@gmail.com>
2020-10-06 11:42:38 +0200

    Updates

---
+++ precache-manifest.a9c5bbe900119a61df34ea0695564d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9b68c8e9251644561f41d9ed7ad717",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5f06a8f9076308514207",
+    "url": "/monster-rolodex/static/css/main.e17d4671.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f06a8f9076308514207",
+    "url": "/monster-rolodex/static/js/main.a282cc74.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
